5996e25d1db78c99220a4a626d5f1b121e602dd4
JessicaDiehl <jessica_diehl@hotmail.com>
2024-04-25 17:11:10 -0300

    alterando catalogo e server para registrar produtos

--- catalogo.go
+++ catalogo.go
@@ -1,73 +1,32 @@
 package main
 
 import (
+	"errors"
+
 	"github.com/JessicaDiehl/loja-digiport-backend/model"
 )
 
-func criaCatalogo() []model.Produto { //funcao retorna um array de produto
-	produtos := []model.Produto{
-		{
-			Id:         "1",
-			Nome:       "Blusa laura",
-			Descricao:  "Cor azul",
-			Categoria:  "Blusa",
-			Valor:      19.90,
-			Quantidade: 2,
-			Imagem:     "azul_laura.png",
-		},
-
-		{
-			Id:         "2",
-			Nome:       "Blusa agata",
-			Descricao:  "Cor agata",
-			Categoria:  "Blusa",
-			Valor:      50.00,
-			Quantidade: 1,
-			Imagem:     "laranja_agata.png",
-		},
-		{
-			Id:         "3",
-			Nome:       "Calça Roberta",
-			Descricao:  "Calça jeans mom",
-			Categoria:  "Calça",
-			Valor:      100.00,
-			Quantidade: 5,
-			Imagem:     "calca_roberta.png",
-		},
-
-		{
-			Id:         "4",
-			Nome:       "Calça Julia",
-			Descricao:  "Calça jeans wide",
-			Categoria:  "Calça",
-			Valor:      100.00,
-			Quantidade: 4,
-			Imagem:     "calca_julia.png",
-		},
-		{
-			Id:         "5",
-			Nome:       "Calça Karol",
-			Descricao:  "Calça jeans mom",
-			Categoria:  "Calça",
-			Valor:      140.00,
-			Quantidade: 3,
-			Imagem:     "calca_karol.png",
-		},
-	}
-
-	return produtos
-}
+var produtos []model.Produto = []model.Produto{}
 
 func produtosPorNome(nome string) []model.Produto {
 
-	produtosCatalogo := criaCatalogo()
+	resultados := []model.Produto{}
 
-	var produtosFiltrados []model.Produto
-
-	for _, produtos := range produtosCatalogo {
-		if produtos.Nome == nome {
-			produtosFiltrados = append(produtosFiltrados, produtos)
+	for _, p := range produtos {
+		if p.Nome == nome {
+			resultados = append(resultados, p)
 		}
 	}
-	return produtosFiltrados
+	return resultados
+}
+
+func registraProduto(produtoNovo model.Produto) error {
+
+	for _, produto := range produtos {
+		if produtoNovo.Id == produto.Id {
+			return errors.New("produto com o ID ja cadastrado")
+		}
+	}
+	produtos = append(produtos, produtoNovo)
+	return nil
 }
--- server.go
+++ server.go
@@ -13,22 +13,32 @@ func StartServer() {
 }
 
 func produtosHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		getProdutos(w, r)
+	} else if r.Method == "POST" {
+		addProduto(w, r)
+	}
+}
 
-	queryParam := r.URL.Query()
+func addProduto(w http.ResponseWriter, r *http.Request) {
+	var produto model.Produto
+	json.NewDecoder(r.Body).Decode(&produto)
 
-	nome := queryParam.Get("nome")
+	registraProduto(produto)
 
-	produtos := criaCatalogo()
+	w.WriteHeader(http.StatusCreated)
+}
+
+func getProdutos(w http.ResponseWriter, r *http.Request) {
+	queryParam := r.URL.Query() //r representa toda a request, dentro da URL tem uma funcao query, retornando as queries passadas.
+
+	nome := queryParam.Get("nome") //especificamos o parametro de query que tem que ser passado
 
 	if nome != "" {
-		filteredProdutos := []model.Produto{}
-		for _, p := range produtos {
-			if p.Nome == nome {
-				filteredProdutos = append(filteredProdutos, p)
-			}
-		}
-		produtos = filteredProdutos
+		produtosFiltradosPorNome := produtosPorNome(nome)
+		json.NewEncoder(w).Encode(produtosFiltradosPorNome)
+	} else {
+		Produtos := produtos
+		json.NewEncoder(w).Encode(Produtos)
 	}
-
-	json.NewEncoder(w).Encode(produtos)
 }
